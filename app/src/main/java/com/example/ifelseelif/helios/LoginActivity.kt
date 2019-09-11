52d8d61b81afe79295a65420c633e980112dd828
ifelseelif <yougrysha@gmail.com>
2019-09-11 17:12:53 +0300

    add onClickListener on reg

--- app/src/main/java/com/example/ifelseelif/helios/LoginActivity.kt
+++ app/src/main/java/com/example/ifelseelif/helios/LoginActivity.kt
@@ -1,12 +1,27 @@
 package com.example.ifelseelif.helios
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        button.setOnClickListener {
+            println(1)
+        }
+
+        helpHelios.setOnClickListener {
+            intent = Intent(Intent.ACTION_VIEW).setData(Uri.parse(R.string.helpHelios.toString()))
+            startActivity(intent)
+        }
     }
+
+
 }
--- app/src/main/java/com/example/ifelseelif/helios/MainActivity.kt
+++ app/src/main/java/com/example/ifelseelif/helios/MainActivity.kt
@@ -1,33 +1,13 @@
 package com.example.ifelseelif.helios
 
 import android.os.Bundle
+import android.widget.Button
 import com.arellomobile.mvp.MvpAppCompatActivity
+import com.example.ifelseelif.helios.R.string.enter
 import com.example.ifelseelif.helios.mvp.views.LoginView
 
-class MainActivity : MvpAppCompatActivity(), LoginView{
-    override fun showErrorPassword() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+class MainActivity : MvpAppCompatActivity(){
 
-    override fun showErrorLogin() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun showSuccess() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun loginByVK() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun loginByGoogle() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun forgotPassword() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
